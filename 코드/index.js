10b4fadcf40fcde7d0e1148d110dd641e2af23ed
yerin_J <86339431+jiyerin@users.noreply.github.com>
2023-06-06 19:18:05 +0900

    HTML,CSS,JS:BOOK

--- 코드/index.js
+++ 코드/index.js
@@ -25,3 +25,37 @@ makeChart(77.322, chart1, '#39C08F');
 makeChart(80.496, chart2, '#39C08F');
 makeChart(79.53, chart3, '#39C08F');
 makeChart(60, chart4, '#dedede');
+
+// book
+var slides = document.querySelector('.book_list'),
+  slide = document.querySelectorAll('.book_list li'),
+  currentIdx = 0,
+  slideCount = slide.length,
+  slideWidth = 300,
+  slideMargin = 30,
+  prevBtn = document.querySelector('.prev'),
+  nextBtn = document.querySelector('.next');
+
+slides.style.width =
+  (slideWidth + slideMargin) * slideCount - slideMargin + 'px';
+
+function moveSlide(num) {
+  slides.style.left = -num * 330 + 'px';
+  currentIdx = num;
+}
+
+nextBtn.addEventListener('click', function () {
+  if( currentIdx < slideCount - 3){
+    moveSlide(currentIdx + 1);
+  }else{
+    moveSlide(0);
+  }   
+});
+
+prevBtn.addEventListener('click', function () {
+  if( currentIdx > 0){
+    moveSlide(currentIdx - 1);
+  }else{
+    moveSlide(slideCount - 3);
+  }   
+});
